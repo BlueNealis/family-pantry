84051ff2d5422a966eaaddb0fe8286c7356624c9
Blue Jessen <canealisblue@gmail.com>
2026-01-08 22:13:23 -0500

    Add base header

---
+++ src/components/headerComponents/header.jsx
@@ -0,0 +1,13 @@
+const header = () => {
+    return (
+        <header>
+            <h1>Family Kitchen</h1>
+            <div>
+                <a href="">Pantry</a>
+                <a href="">Cookbook</a>
+            </div>
+        </header>
+    )
+}
+
+export default header;
