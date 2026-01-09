c3f24d9da5a418e5e0d5201a09f6400d04bfb386
Blue Jessen <canealisblue@gmail.com>
2026-01-08 22:51:04 -0500

    Show items in pantry

--- src/app/pantry/page.js
+++ src/app/pantry/page.js
@@ -4,14 +4,16 @@ export default function Pantry() {
     return(
         <div>
             <h1>Pantry</h1>
-            {pantryItems.map((item)=> {
-                <ItemCard 
-                    id={item.id} 
-                    name={item.name}
-                    buyDate={item.buyDate}
-                    quantity={item.quantity}
-                    />
-            })}
+            <div className="pantry-container">
+                {pantryItems.map((item)=> {
+                    return (<ItemCard 
+                        id={item.id} 
+                        name={item.name}
+                        buyDate={item.buyDate}
+                        quantity={item.quantity}
+                        />)
+                })}
+            </div>
         </div>
     )
 }
