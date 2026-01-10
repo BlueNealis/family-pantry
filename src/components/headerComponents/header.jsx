445d6b2fb54ed71af4ed4a7ff04c61ecd529a01b
Blue Jessen <canealisblue@gmail.com>
2026-01-09 22:03:25 -0500

    Add sass

--- src/components/headerComponents/header.jsx
+++ src/components/headerComponents/header.jsx
@@ -1,8 +1,10 @@
+import styles from './Header.module.scss';
+
 const header = () => {
     return (
         <header>
             <h1>Family Kitchen</h1>
-            <div>
+            <div className={styles.navBar}>
                 <a href="/pantry">Pantry</a>
                 <a href="/cookbook">Cookbook</a>
             </div>
